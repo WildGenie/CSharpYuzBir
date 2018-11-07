ff536535631c488a2ac5fecab6c64dbc986b2563
Bilgehan Zeki ÖZAYTAÇ <wildgenie@outlook.com>
2018-11-07 10:26:43 +0300

    int türü ile toplama operatorü örneği hazırlandı.

--- CsharpYuzBir/Program.cs
+++ CsharpYuzBir/Program.cs
@@ -14,11 +14,19 @@ namespace CsharpYuzBir
             //Console.WriteLine("Yazar: Reşat Nuri");
             //Console.WriteLine("Tür: Roman");
 
-            string cumle, cumle2;
-            cumle = "Bugün Tarih 7 Kasım 2018";
-            cumle2 = "Bu gün hava sıcak";
-            Console.Write(cumle + " " + cumle2);
+            //string cumle, cumle2;
+            //cumle = "Bugün Tarih 7 Kasım 2018";
+            //cumle2 = "Bu gün hava sıcak";
+            //Console.Write(cumle + " " + cumle2);
 
+            //int sayi = 24;
+            //Console.Write(sayi);
+
+            int sayi1, sayi2, sonuc;
+            sayi1 = 20;
+            sayi2 = 30;
+            sonuc = sayi1 + sayi2;
+            Console.Write(sonuc);
 
             Console.Read();
         }
